559af2b8cf2377248c31381e9b544ef0aea93912
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-07 11:04:02 +0700

    VerifyEmail controller complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,8 +34,21 @@ const register = async (req, res) => {
 
 const verifyEmail = async (req, res) => {
   const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
 
-  res.status(StatusCodes.OK).json({ verificationToken, email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = "";
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Email Verified" });
 };
 
 const login = async (req, res) => {
