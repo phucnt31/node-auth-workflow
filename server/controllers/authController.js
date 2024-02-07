cb56f507df6a50cca89270c50bc0ff53916cb01a
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-07 14:32:21 +0700

    Send reset password email

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,6 +6,7 @@ const {
   attachCookiesToResponse,
   createTokenUser,
   sendVerificationEmail,
+  sendResetPasswordEmail,
 } = require("../utils");
 const crypto = require("crypto");
 const sendEmail = require("../utils/sendEmail");
@@ -136,6 +137,14 @@ const forgotPassword = async (req, res) => {
   if (user) {
     const passwordToken = crypto.randomBytes(70).toString("hex");
     // send email
+    const origin = "http://localhost:3000";
+    await sendResetPasswordEmail({
+      name: user.name,
+      email: user.email,
+      token: passwordToken,
+      origin,
+    });
+
     const tenMinutes = 1000 * 60 * 10;
     const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
 
