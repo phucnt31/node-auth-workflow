65ce836acca7e0b420e5f7a675f71ffdfdd5250c
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-02-07 13:11:53 +0700

    Create token in login controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Token = require("../models/Token");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const {
@@ -81,9 +82,20 @@ const login = async (req, res) => {
     throw new CustomError.UnauthenticatedError("Please verify your email");
   }
   const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
 
-  res.status(StatusCodes.OK).json({ user: tokenUser });
+  // create refresh token
+  let refreshToken = "";
+  // check for existing token
+  refreshToken = crypto.randomBytes(40).toString("hex");
+  const userAgent = req.headers["user-agent"];
+  const ip = req.ip;
+  const userToken = { refreshToken, ip, userAgent, user: user._id };
+
+  const token = await Token.create(userToken);
+
+  // attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser, token });
 };
 
 const logout = async (req, res) => {
